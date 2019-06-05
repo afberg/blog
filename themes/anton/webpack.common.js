0cb63df6d97f0b07c6a9521b4d68a6cddfebd47c
Anton Forsberg <anton.forsberg@valtech.com>
2019-06-05 09:36:33 +0200

    updates webpack config

--- themes/anton/webpack.common.js
+++ themes/anton/webpack.common.js
@@ -1,42 +1,46 @@
 const path = require("path");
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-module.exports = {
-    entry: {
-        index: './src/css/index.css'
-    },
-    output: {
-        filename: '[name].js',
-        path: path.resolve(__dirname, './dist/')
-    },
-    module: {
-        rules: [
-          {
-            test: /\.css$/,
-            
-            use: [
-                {
-                    loader: MiniCssExtractPlugin.loader,
-                    options: {
-                      hmr: true,
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+module.exports = env => {
+    console.log('NODE_ENV: ', env.NODE_ENV);
+    console.log('Production: ', env.production);
+    return {
+        mode: env.production ? "production": "development",
+        entry: {
+            home: './src/home'
+        },
+        output: {
+            filename: '[name].[chunkhash].js',
+            path: path.resolve(__dirname, './dist/')
+        },
+        module: {
+            rules: [{
+                test: /\.css$/,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                        options: {
+                            hmr: true,
+                        },
                     },
-                },
-                {
-                    loader: 'css-loader',
-                    options: {
-                    importLoaders: 1
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            importLoaders: 1
+                        }
+                    },
+                    {
+                        loader: 'postcss-loader'
                     }
-                },
-                {
-                    loader: 'postcss-loader'
-                }
-            ]
-          }
-        ]
-    },
-    plugins: [
-        new MiniCssExtractPlugin({
-            filename: '[name].[contenthash].css'
-        }),
-    ],
+                ]
+            }]
+        },
+        plugins: [
+            new CleanWebpackPlugin(),
+            new MiniCssExtractPlugin({
+                filename: '[name].[contenthash].css'
+            }),
+        ],
+    }
 };
 
