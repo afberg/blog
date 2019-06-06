2ae757bc5a69baec19b8012b44778c403bdf2fb5
Anton Forsberg <anton.forsberg@valtech.com>
2019-06-06 11:52:13 +0200

    Implements fully working animating text on start page

--- src/home/animated-text.ts
+++ src/home/animated-text.ts
@@ -1,40 +1,57 @@
 import {LitElement, html, css, customElement, property} from 'lit-element';
 
-// This decorator defines the element.
 @customElement('animated-text')
 export class AnimatedText extends LitElement {
 
-    // This decorator creates a property accessor that triggers rendering and
-    // an observed attribute.
     @property({ type: String}) text = 'Anton_Forsberg';
     @property({ type: Number}) visible = 0;
 
+    constructor() {
+        super();
+      }
+
     static get styles() {
         return css`
+        :host {
+            display: inline-flex;
+            border-bottom: 1px solid var(--textColor);
+        }
         .container {
             display: flex;
+            
+        }
+        .container.animating {
+            cursor: pointer;
+        }
+        .animating span.transform {
+            color: var(--accentColor);
         }
         span {
             font-family: monospace;
             display: flex;
             color: var(--textColor);
-        }
-        .char {
-            animation: typeIn 0.5s ease-in-out;
-            animation-fill-mode: forwards;
+            font-size: var(--fontSize);
         }
         .transform{
             max-width: 0ch;
             overflow: hidden;
             opacity: 0;
-            background-color: var(--textColor);
-            color: white;
+            transition: color 2s ease-out;
+        }
+        .animate {
+            animation: typeIn 0.5s ease-in-out;
+            animation-fill-mode: forwards;
+        }
+        .transform.last {
+            max-width: unset;
+            opacity: 1;
+            background-color: transparent;
         }
 
         @keyframes typeIn {
+            from {background-color: var(--textColor)}
             to {
                 opacity: 1;
-                color: var(--textColor);
                 background-color: transparent;
                 max-width: 1ch;
             }
@@ -42,22 +59,40 @@ export class AnimatedText extends LitElement {
         `;
         
     }
-
-    // Render element DOM by returning a `lit-html` template.
-    render() {
-    return html`
-    <div class="container">
-        ${this.text.split('').map( (char, i) => html`
-        ${this.visible < this.text.length - 1 && i === this.text.length - 1 ? html`
-            <span class="number">${this.text.length - 2 - this.visible}</span>` : ''
+    startAnimation() {
+       this.updateVisible();
+    }
+    updateVisible() {
+        const timeout = Math.max(0.3,Math.random())*800;
+        if(this.visible < this.text.length - 1) {
+            setTimeout(() => {
+                this.visible++;
+                this.updateVisible();
+            }, timeout)
         }
-            <span class="char 
-                ${ i > this.visible && i < this.text.length - 1 ? 'transform' : ''}"
-                style="animation-delay: ${i*0.5+Math.random() + 's'};">
-                ${char}
-            </span>
-        `)}
-    </div>`;
+        
+    }
+
+    render() {
+        return html`
+        <div class="container ${this.visible < this.text.length -1 ? 'animating': ''}"
+            @click="${() => this.startAnimation()}">
+            ${this.text.split('').map( (char, i) => html`
+                <span class="char 
+                    ${ this.shouldTransform(i) ? 'transform' : ''}
+                    ${ this.shouldTransform(i) && i <= this.visible ? 'animate': ''}
+                    ${ this.isLast(i) ? 'last' : ''}">
+                    ${ this.isLast(i) &&  this.visible < i ? this.text.length - this.visible - 2 : char }
+                </span>
+            `)}
+        </div>`;
+    }
+
+    isLast(index: number): boolean {
+        return index === this.text.length - 2;
+    }
+    shouldTransform(index: number): boolean {
+        return index > 0 && index < this.text.length - 1;
     }
 
 }
